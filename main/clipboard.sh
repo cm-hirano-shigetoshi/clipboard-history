4fbd8c8265dc9d67d9db4fb267b13005cd7f2df5
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-11-21 16:28:58 +0900

    fzfymlの新バージョン対応

--- main/clipboard.sh
+++ main/clipboard.sh
@@ -3,24 +3,23 @@ readonly CLIPBOARD_HISTORY_FILE="${HOME}/.clipboard_history"
 readonly TOOL_DIR="$(dirname $(perl -MCwd=realpath -le 'print realpath shift' "$0"))"
 echo $TOOL_DIR >> ~/.debug
 result=$(fzfyml run ${TOOL_DIR}/clipboard.yml "$CLIPBOARD_HISTORY_FILE")
-if [[ -z "${result}" ]]; then
-  return
-fi
-type=$(head -1 <<< "${result}")
-if [[ "${type}" = "clipboard" ]]; then
-  sed '1d' <<< "${result}" \
-    | tr '' '\n' \
-    | perl ${TOOL_DIR}/del_newline.pl \
-    | pbcopy
-elif [[ "${type}" = "edit" ]]; then
-  temp_file=$(mktemp "/tmp/clipboard_history.XXXXXX")
-  sed '1d' <<< "${result}" \
-    | tr '' '\n' \
-    | perl ${TOOL_DIR}/del_newline.pl \
-    > ${temp_file}
-  ${EDITOR-vim} ${temp_file}
-  if [[ -s ${temp_file} ]]; then
-    cat ${temp_file} | pbcopy
+if [[ -n "${result}" ]]; then
+  type=$(head -1 <<< "${result}")
+  if [[ "${type}" = "clipboard" ]]; then
+    sed '1d' <<< "${result}" \
+      | tr '' '\n' \
+      | perl ${TOOL_DIR}/del_newline.pl \
+      | pbcopy
+  elif [[ "${type}" = "edit" ]]; then
+    temp_file=$(mktemp "/tmp/clipboard_history.XXXXXX")
+    sed '1d' <<< "${result}" \
+      | tr '' '\n' \
+      | perl ${TOOL_DIR}/del_newline.pl \
+      > ${temp_file}
+    ${EDITOR-vim} ${temp_file}
+    if [[ -s ${temp_file} ]]; then
+      cat ${temp_file} | pbcopy
+    fi
   fi
 fi
 
