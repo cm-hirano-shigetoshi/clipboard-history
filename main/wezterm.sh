2514f2bd0c08dacd577c9c0e5402d202570c4263
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-09-24 14:00:26 +0900

    coc用にnodeにPATHを通す

--- main/wezterm.sh
+++ main/wezterm.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# WezTermで直接プロセス起動した場合zshrcが読まれない
 PATH="$PATH:/opt/homebrew/bin"
+PATH="$PATH:$HOME/.local/share/mise/installs/node/latest/bin"
 
 readonly TOOL_DIR="$(dirname $(perl -MCwd=realpath -le 'print realpath shift' "$0"))"
 bash "$TOOL_DIR/../main/clipboard.sh"
