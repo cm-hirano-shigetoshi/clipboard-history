6cc13da58ba703b00639693999f824991e71172a
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-10-10 00:23:55 +0900

    元のペインに戻る機能

--- main/wezterm_edit.sh
+++ main/wezterm_edit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+PREV_PANE_ID="$1"
+trap "$WEZTERM_EXECUTABLE_DIR/wezterm cli activate-pane --pane-id $PREV_PANE_ID" EXIT
+
 # WezTermで直接プロセス起動した場合zshrcが読まれない
 PATH="$PATH:/opt/homebrew/bin"
 PATH="$PATH:$HOME/.local/share/mise/installs/node/latest/bin"
